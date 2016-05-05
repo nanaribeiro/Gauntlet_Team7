406101f36a276d4a689e828a9d052da33d95cbe6
cjapp <cjapp@mail.csuchico.edu>
2016-05-05 09:31:13 -0700

    Added pseudo-code to enemy script and enemy spawner

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	//public variables
+	public Transform target;
+	public int health;
+	
+	//private variables
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//run Movement script
+		//for other enemies, run their scripts (like ghost kamikaze, grunt melee, lobber ranged attack, etc)
+	}
+
+	//want the enemies movment to be grid like
+	void Movement()
+	{
+		//if the tile the enemy wants to move in is available
+		//and they are not in the middle of a move coroutine
+
+			//if player closest is not target player
+				//change closest to the target player
+			//run the move couroutine
+
+			//do nothing
+	}
+}
--- Assets/Scripts/Enemy_Spawner.cs
+++ Assets/Scripts/Enemy_Spawner.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public enum Type_spawn
+{
+	Ghost,Grunt,Lobber,Sorcerer,Demon,Thief
+}
 
 public class Enemy_Spawner : MonoBehaviour {
 
+	//public variables
+	public Type_spawn m_spawn_type;
+	public List<GameObject> _m_spawnable_enemies;
+	public int m_health;
+
+	//private variables
+	private GameObject enemy_type;
+
 	// Use this for initialization
-	void Start () {
-	
+	void awake() {
+		//determine what enemy type to spawn
+
+			//set the enemy_type prefab to enemy type spawn
+
+			//change skin of the spawner to match enemy's skin
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void spawn_enemy()
+	{
+		//check if there is suitable space to spawn an enemy
+			//spawn an enemy with relative strength to the spawner's health
+	}
+
+	//this will be the function that checks neighboring slots
+	//for valid summoning of enemies
+	bool check_empty_nearby()
+	{
+		//iterate through neighboring locations
+			//if space is available return true
+			return true;
+
+		//return false
 	}
 }
