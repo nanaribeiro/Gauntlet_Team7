964a93204dfe9dced8aa297efadb7944763eead2
nanaribeiro <alanaribeiroramos@gmail.com>
2016-05-17 01:34:08 -0700

    Aded exit script and level 1 maze

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Player") {
+			int i = Application.loadedLevel;
+			Application.LoadLevel(i+1);
+		}
+	}
+}
