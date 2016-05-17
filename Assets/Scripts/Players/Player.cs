43b0d3d9bb5d7b30eee7026a66866b0063a481b2
nanaribeiro <alanaribeiroramos@gmail.com>
2016-05-17 00:12:52 -0700

    Added public variables to player script

--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -10,10 +10,10 @@ public class Player : MonoBehaviour {
 	public GameObject shot;
 	public Transform shotSpawn;
 	public float moveSpeed = 2.5f;
-	/*public float minX = -8.27f;
-	public float maxX = 8.27f;
-	public float minY = -3.5f;
-	public float maxY = 5.41f;*/
+	public int health;
+	public int score;
+	public int damage;
+	public int potion;
 
 	protected void Movement () {
 		horizontal = Input.GetAxis ("Horizontal");
